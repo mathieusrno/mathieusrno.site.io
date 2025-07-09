8cfb8e232098f73f3229589a2b7836687188270e
Mathieu Surinot <matsor@outlook.fr>
2025-07-09 09:06:43 +0100

    Modif

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-<script>*
+window.onload = function() {
     function affiche(id) {
-
         let contenu = document.getElementById("contenu");
 
         if (id == "texte_1") {
             contenu.innerHTML = "paragraphe 1"
         } else if (id == "texte_2") {
             contenu.innerHTML = "paragraphe 2"
-        } else if (id == texte_3) {
+        } else if (id == "texte_3") {
             contenu.innerHTML = "paragraphe 3"
         }
 
     }
-
     
-</script>
+}
