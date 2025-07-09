5966a49f8d14fa428acdcd19af6430e92068cfa9
Mathieu Surinot <matsor@outlook.fr>
2025-07-09 09:12:21 +0100

    Modif

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-window.onload = function() {
-    function affiche(id) {
-        let contenu = document.getElementById("contenu");
 
-        if (id == "texte_1") {
-            contenu.innerHTML = "paragraphe 1"
-        } else if (id == "texte_2") {
-            contenu.innerHTML = "paragraphe 2"
-        } else if (id == "texte_3") {
-            contenu.innerHTML = "paragraphe 3"
-        }
+function affiche(id) {
+    let contenu = document.getElementById("contenu");
 
+    if (id == "texte_1") {
+        contenu.innerHTML = "paragraphe 1"
+    } else if (id == "texte_2") {
+        contenu.innerHTML = "paragraphe 2"
+    } else if (id == "texte_3") {
+        contenu.innerHTML = "paragraphe 3"
     }
-    
+
 }
+    
